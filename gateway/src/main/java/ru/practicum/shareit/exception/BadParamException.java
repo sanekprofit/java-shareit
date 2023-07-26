d3fc823b45ad8a31f1f214552c1dd67a3dc8f5ae
sanekprofit <zaev-alex2006@yandex.ru>
2023-07-26 06:10:40 +0300

    style: BadParamException fix

--- gateway/src/main/java/ru/practicum/shareit/exception/BadParamException.java
+++ gateway/src/main/java/ru/practicum/shareit/exception/BadParamException.java
@@ -1,5 +1,7 @@
 package ru.practicum.shareit.exception;
 
 public class BadParamException extends RuntimeException {
-    public BadParamException(String s) {super(s);}
+    public BadParamException(String s) {
+        super(s);
+    }
 }
